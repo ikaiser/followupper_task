d72c3b3dfa7c3b1189a22cd8dfaad726a1381bd8
Lorenzo Pedà <lorenzo.peda@gmail.com>
2020-07-20 11:56:31 +0200

    Added Typology Section

---
+++ app/Http/Controllers/TypologyController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Typology;
+use Illuminate\Http\Request;
+
+class TypologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $typology = Typology::all();
+        return view('quotations.typology.index', ['typology' => $typology]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('quotations.typology.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'typology' => 'required',
+        ]);
+
+        $typology = new Typology();
+
+        $typology->name = $request->get('typology');
+        $typology->save();
+
+        return redirect()->route('quotations_typology.index')->with('message', __('Typology Added Successfully!'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param $typology_id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($typology_id)
+    {
+        $typology = Typology::find($typology_id);
+
+        return view('quotations.typology.edit', ['typology' => $typology]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param $typology_id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, $typology_id)
+    {
+        $request->validate([
+            'typology' => 'required',
+        ]);
+
+        $typology = Typology::find($typology_id);
+        $typology->name = $request->get('typology');
+        $typology->save();
+
+        return redirect()->route('quotations_typology.index')->with('message', __('Typology Updated Successfully!'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param $typology_id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($typology_id)
+    {
+        $typology = Typology::find($typology_id);
+
+        if(is_null($typology))
+        {
+            return redirect()->route('quotations_typology.index')->with('error', __('Typology Already Deleted or not Existing'));
+        }
+
+        $typology->delete();
+
+        return redirect()->route('quotations_typology.index')->with('message', __('Typology Deleted Successfully!'));
+    }
+}
---
+++ resources/views/quotations/typology/edit.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <div class="row">
+        <div class="col s12 m6 mt-4">
+            <h6>
+                <a href="#" class="pointer">Home</a>
+                <a onclick="document.location.href='{{ route('quotations.index') }}'" class="pointer">&nbsp;/&nbsp;@lang('Quotations')</a>
+                <a onclick="document.location.href='{{ route('quotations_status.index') }}'" class="pointer">&nbsp;/&nbsp;@lang('Typology')</a>
+                <a href="#" class="pointer"> / @lang('Edit Typology') </a>
+            </h6>
+        </div>
+        <div class="col s12 m6 mt-4 right-align">
+            <button class="btn btn-floating waves-effect waves-light" onclick="window.history.back()"><i class="material-icons">arrow_back</i></button>
+        </div>
+    </div>
+
+    <div class="card hoverable">
+        <div class="card-content">
+            <span class="card-title"> @lang('Edit Typology') </span>
+
+            @if ($errors->any())
+                <div class="card-alert card red lighten-5 my-4">
+                    <div class="card-content red-text">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <button type="button" class="close red-text" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+            @endif
+
+            <div class="row">
+                <div class="col s12">
+                    <div class="card">
+                        <form method="post" action="{{ route('quotations_typology.update', [$typology->id]) }}" enctype="multipart/form-data">
+                            @csrf
+                            <div class="card-content">
+                                <div class="row">
+                                    <div class="col l12">
+                                        <div class="input-field my-3">
+                                            <label for="typology"> @lang('Typology') </label>
+                                            <input type="text" name="typology" value="{{$typology->name}}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn waves-effect waves-light mt-3"> @lang('Update') </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('js')
+    @parent
+@endsection
